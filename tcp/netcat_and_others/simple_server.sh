99aa05b6823ba913efcb2992c9a3016625bee1ca
Zev Averbach <zev@averba.ch>
2022-04-18 22:56:44 +0200

    added some response headers, removed extraneous line break, added blank 'echo' in order for the response text to be interpreted by clients as such.

--- tcp/netcat_and_others/simple_server.sh
+++ tcp/netcat_and_others/simple_server.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
+
+#### INPUT
+# read stdin which, when this is run with `socat`, will be an incoming request
 read MESSAGE
+# split the message on spaces
 # spaces are the default delimiter, but we're being explicit here
 IFS=' ' read -r -a MESSAGE_ARRAY <<< "$MESSAGE"
 HTTP_METHOD=${MESSAGE_ARRAY[0]}
 ENDPOINT=${MESSAGE_ARRAY[1]}
+# TODO: check other headers, maybe even including authentication and authorization
+#### /INPUT
+
+#### OUTPUT
+STATUS_LINE="HTTP/1.1 "
 if [[ "$ENDPOINT" == "/hi" ]]; then
-  RESPONSE="oh hi"
+  STATUS_LINE+="200 OK"
+  RESPONSE="Hi There"
 else 
-  RESPONSE="not found!"
+  STATUS_LINE+="400 NOT FOUND"
+  RESPONSE=""
+# TODO: produce other responses
 fi
-echo "HTTP/1.1 200\n$RESPONSE"
+
+RESPONSE_LENGTH=$(echo $RESPONSE | wc -c)
+# this is necessary maybe because of the omission of a newline below with 'echo -n'
+# without it, this appears: "curl: (18) transfer closed with 1 bytes remaining to read"
+((RESPONSE_LENGTH=RESPONSE_LENGTH-1))
+
+echo $STATUS_LINE
+echo "Content-Type: text/html; charset=utf-8"
+echo "Content-Length: $RESPONSE_LENGTH"
+echo ""
+echo -n $RESPONSE
+# TODO: add Date and Server headers, at least
+#### /OUTPUT
